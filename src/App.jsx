8a6be2f5bab3b9254c916e2836acdf2076ad5147
Chalitha-Aluwihare <chalithaaluwihare@gmail.com>
2024-12-12 15:55:34 +0530

    implemented dropdown menu with toggle functionality, finalized design (non-responsive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Offers from "./components/Offers";
+import Ratings from "./components/Ratings";
+import Price from "./components/Price";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className="relative flex items-center justify-center h-screen gap-3">
+      <div>
+        <Ratings />
+      </div>
+      <div>
+        <Price />
+      </div>
+      <div>
+        <Offers />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
